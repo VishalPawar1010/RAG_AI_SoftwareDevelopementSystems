ba0047c75d5d5ecd38ebfff480a1327a4a5bff71
Vishal Pawar <vshalofficla@gmail.com>
2024-08-17 01:35:26 +0530

    added analytics

--- rag-poc-01/frontend/app/layout.tsx
+++ rag-poc-01/frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "./markdown.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}
       <SpeedInsights />
+      <Analytics />
+
       </body>
     </html>
   );
